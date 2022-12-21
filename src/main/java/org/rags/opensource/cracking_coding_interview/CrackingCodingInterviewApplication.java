dd3a756b82cc6a0f167d8c3fbc85d98f9c4971ba
raghavendragr <raghugr16@gmail.com>
2022-12-21 12:46:01 +0530

    Initial commit

---
+++ src/main/java/org/rags/opensource/cracking_coding_interview/CrackingCodingInterviewApplication.java
@@ -0,0 +1,13 @@
+package org.rags.opensource.cracking_coding_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackingCodingInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackingCodingInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rags/opensource/cracking_coding_interview/CrackingCodingInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rags.opensource.cracking_coding_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackingCodingInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
